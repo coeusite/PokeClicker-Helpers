f0b07f25548dab3186d06dda8e40f71197bc1561
CoeusITE <coeusite@posteo.org>
2021-06-18 09:21:50 +0800

    Compatible wit v0.8.2

--- pokeEggs.js
+++ pokeEggs.js
@@ -1,30 +1,30 @@
 function addEgg(){
-    for( var i = 0; i<player.caughtPokemonList.length; i++){
-		if(canBreed(player.caughtPokemonList[i]) && breedSlotLeft()){
-		    //update to bypass mom
-            poke = player.caughtPokemonList[i].name
-                gainPokemonEgg(poke)
-                releasePokemon(poke)
+    for( var i = 0; i<App.game.party.caughtPokemon.length; i++){
+    	var poke = App.game.party.caughtPokemon[i]
+		if(App.game.breeding.canBreedPokemon() && BreedingController.visible(poke)()){
+		    // bypass mom using embedded filters and custom categories
+                    App.game.breeding.addPokemonToHatchery(poke)
+                    console.log("addPokemonToHatchery:", poke.name, poke.level, poke.breeding, PokemonHelper.calcNativeRegion(poke.name))
 		}
 	}
 }
 
 function hatchEggs(){
-    for(var i = 0; i<player.eggList.length; i++){
-		var egg = player.eggList[i];
+	for(var i = App.game.breeding._eggList.length-1; i>=0; i--){
+		var egg = App.game.breeding._eggList[i]();
 		if(egg !== null){
-			if( egg.progress >= egg.steps){
-			    hatchEgg(i)
+			if( egg.canHatch()){
+			    App.game.breeding.hatchPokemonEgg(i)
+			    console.log("hatchPokemonEgg:", egg.pokemon)
 			}
 		}
     }
 }
 
 function loopEggs(){
-
     var eggLoop = setInterval(function() {
+    	console.log("looping Eggs:")
         hatchEggs()
         addEgg()
     }, 6000)
 }
-
