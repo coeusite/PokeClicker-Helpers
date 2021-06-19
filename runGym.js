c08c96ee8abd0367786b1af7a85fcf50a330ef02
CoeusITE <coeusite@posteo.org>
2021-06-19 11:40:50 +0800

    loopGym rewrite
    loopGym('', 10) for towns, loopGym('???', 10) for elites,

--- runGym.js
+++ runGym.js
@@ -1,25 +1,44 @@
-function loopGym(gymName, times)  {
-    loadGym(gymName)  
-    var times = times
+function loadGym(leaderName) {
+    if (player.town().gym){
+        var gym = player.town().gym
+    } else if ('gymList' in player.town()) {
+        var gym = searchGymList(leaderName)
+    }
+    if (gym) {
+        GymRunner.startGym(gym)
+        console.log('loading Gym:', gym.leaderName)
+    } else {
+        console.log('This Gym does not exist:', leaderName)
+    }
+}
 
+function searchGymList(leaderName){
+    for (var i=0; i < player.town().gymList.length; i++) {
+        if (player.town().gymList[i].leaderName === leaderName) {
+            return player.town().gymList[i];
+        }
+    }
+}
 
+function loopGym(leaderName, times = 1)  {
+    console.log("entering loop")
+    var times = Math.floor(times)
     var looper = setInterval(function() {
         //if still have loops, and currentGym.pokemons[gymPokemonIndex] != null, continue
         //if all pokemon beat, one less times
         //if no times, clearInterval(looper)
-        console.log("entered loop")
-        if(times != 0 && currentGym.pokemons[gymPokemonIndex] != null){
+        if(times > 0 && App.game.gameState == GameConstants.GameState.gym){
 
         }
-        if(times != 0 &&currentGym.pokemons[gymPokemonIndex] == null){
+        if(times > 0 && App.game.gameState != GameConstants.GameState.gym){
             times = times - 1
-            $("#gymModal").modal('hide');
-            loadGym(gymName)
+            // $("#gymModal").modal('hide');
+            loadGym(leaderName)
         }
-        if(times == 0 && currentGym.pokemons[gymPokemonIndex] == null){
+        if(times <= 0 && App.game.gameState != GameConstants.GameState.gym){
+            // $("#gymModal").modal('hide');
             console.log("done")
             clearInterval(looper)
-            $("#gymModal").modal('hide');
         }
 
     }, 600)
