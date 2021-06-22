35147d35ffce7d698cb8b2613b1fc1d63438af46
CoeusITE <coeusite@posteo.org>
2021-06-22 12:21:20 +0800

    Update runGym.js

--- runGym.js
+++ runGym.js
@@ -6,28 +6,30 @@ function loadGym(gymName) {
     }
     if (gym) {
         GymRunner.startGym(gym)
-        console.log('loading Gym:', gym.leaderName)
+        console.log('loading Gym:', gym.leaderName, loopTimesGym)
     } else {
         console.log('This Gym does not exist:', gymName)
     }
 }
 
+var loopTimesGym = 0
+
 function loopGym(gymName, times = 1)  {
     console.log("entering loop")
-    var times = Math.floor(times)
+    loopTimesGym = Math.floor(times)
     var looper = setInterval(function() {
         //if still have loops, and currentGym.pokemons[gymPokemonIndex] != null, continue
         //if all pokemon beat, one less times
         //if no times, clearInterval(looper)
-        if(times > 0 && App.game.gameState == GameConstants.GameState.gym){
+        if(loopTimesGym > 0 && App.game.gameState == GameConstants.GameState.gym){
 
         }
-        if(times > 0 && App.game.gameState != GameConstants.GameState.gym){
-            times = times - 1
+        if(loopTimesGym > 0 && App.game.gameState != GameConstants.GameState.gym){
+            loopTimesGym = loopTimesGym - 1
             // $("#gymModal").modal('hide');
             loadGym(gymName)
         }
-        if(times <= 0 && App.game.gameState != GameConstants.GameState.gym){
+        if(loopTimesGym <= 0 && App.game.gameState != GameConstants.GameState.gym){
             // $("#gymModal").modal('hide');
             console.log("done")
             clearInterval(looper)
